9fbb503f29f11ffbebe43e8460da9e9355336f10
TRESOR-TDZ <zignatresor@gmail.com>
2026-05-28 16:59:13 +0100

    changement de port dans le fichier test de connection

--- database/test_db.py
+++ database/test_db.py
@@ -1,28 +1,13 @@
-# from sqlalchemy import create_engine
-
-# DB_URL = "postgresql://admin:admin123@localhost:5432/vehicle_db"
-
-# engine = create_engine(DB_URL)
-
-# try:
-#     connection = engine.connect()
-#     print("Connexion PostgreSQL réussie !")
-#     connection.close()
-
-# except Exception as e:
-#     print("Erreur :", e)
-
 from sqlalchemy import create_engine
 
 DB_URL = "postgresql://admin:admin123@localhost:5432/vehicle_db"
+
 engine = create_engine(DB_URL)
 
 try:
     connection = engine.connect()
-    print("Connexion PostgreSQL réussie !")
+    print("Connexion PostgreSQL reussie !")
     connection.close()
+
 except Exception as e:
-    print("--- UNE ERREUR EST SURVENUE ---")
-    # On force l'extraction du message d'erreur original en ignorant l'erreur d'accent
-    error_message = str(e).encode('utf-8', errors='ignore').decode('utf-8')
-    print(error_message)
+    print("Erreur :", e)
